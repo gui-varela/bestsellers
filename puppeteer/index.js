2c47f789e83c9e5e6cdf886a49db3f1aeb3df56d
gvarela <guilhermevareladossantos@gmail.com>
2023-11-28 20:19:28 -0300

    Corrige erro ao buscar produto sem preço
    #2

--- puppeteer/index.js
+++ puppeteer/index.js
@@ -1,64 +1,68 @@
-// Import puppeteer
 import puppeteer from 'puppeteer'
 
 export const allProducts = (async () => {
 
-	const browser = await puppeteer.launch({ headless: false })
-	const page = await browser.newPage();
-	await page.goto('https://www.amazon.com.br/bestsellers')
+    const browser = await puppeteer.launch({ headless: false })
+    const page = await browser.newPage();
+    await page.goto('https://www.amazon.com.br/bestsellers')
 
-	// Preferi deixar este reload porque a Amazon não permite fazer muitas requisições em sequência
-	await page.reload()
+    // Preferi deixar este reload porque a Amazon não permite fazer muitas requisições em sequência
+    await page.reload()
 
-	const categoryLinks = await page.$$eval('#zg_left_col2 a', element => element.map(link => {
-		const linkInfos = {
-			name: link.innerText,
-			link: link.href
-		}
+    const categoryLinks = await page.$$eval('#zg_left_col2 a', element => element.map(link => {
+        const linkInfos = {
+            name: link.innerText,
+            link: link.href
+        }
+        return linkInfos
+    }))
 
-		return linkInfos
-	}))
+    let sections = []
 
-	let sections = []
+    for (const link of categoryLinks) {
+        await page.goto(link.link)
+        await page.reload()
+        await page.waitForSelector('.p13n-desktop-grid')
 
-	for (const link of categoryLinks) {
-		await page.goto(link.link)
-		await page.reload()
-		await page.waitForSelector('.p13n-desktop-grid')
+        let counter = 0
+        let products = []
 
-		let i = 0
-		let products = []
+        const totalProductsInPage = await page.$$eval('#gridItemRoot', element => element)
 
-		const totalProductsInPage = await page.$$eval('#gridItemRoot', element => element)
+        while (counter < (totalProductsInPage.length >= 3 ? 3 : totalProductsInPage.length)) {
+            const productName = await page.$eval(`#p13n-asin-index-${counter} .p13n-sc-uncoverable-faceout span`, element => element.innerText)
+            const productGrade = await page.$eval(`#p13n-asin-index-${counter} .p13n-sc-uncoverable-faceout .a-row i.a-icon span`, element => element.innerText)
 
-		while (i < (totalProductsInPage.length >= 3 ? 3 : totalProductsInPage.length)) {
-			const productName = await page.$eval(`#p13n-asin-index-${i} .p13n-sc-uncoverable-faceout span`, element => element.innerText)
-			const productGrade = await page.$eval(`#p13n-asin-index-${i} .p13n-sc-uncoverable-faceout .a-row i.a-icon span`, element => element.innerText)
-			const productPrice = await page.$eval(`#p13n-asin-index-${i} .p13n-sc-uncoverable-faceout .a-row .a-size-base span`, element => element.innerText)
+            const productPrice = await page.evaluate((counter) => {
+                const price = document.querySelector(`#p13n-asin-index-${counter} .p13n-sc-uncoverable-faceout .a-row .a-size-base span`)
+                if (!price) {
+                    return "Indisponível"
+                }
+                return price.innerText
+            }, counter)
 
-			const product = {
-				position: i + 1,
-				name: productName,
-				grade: productGrade,
-				price: productPrice,
-				category: link.name
-			}
+            const product = {
+                position: counter + 1,
+                name: productName,
+                grade: productGrade,
+                price: productPrice,
+                category: link.name
+            }
 
-			products.push(product)
+            products.push(product)
+            counter++
+        }
 
-			i++
-		}
+        const section = {
+            name: link.name,
+            products,
+        }
 
-		const section = {
-			name: link.name,
-			products,
-		}
+        sections.push(section)
+        console.log(section)
+    };
 
-		sections.push(section)
-		console.log(section)
-	};
+    await browser.close()
 
-	await browser.close()
-
-	return sections
+    return sections
 })();
