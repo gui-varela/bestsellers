7b4e8d242f4ef52bd543362e18989089612e1930
gvarela <guilhermevareladossantos@gmail.com>
2023-11-30 22:25:07 -0300

    Cria versão inicial da documentação da aplicação
    Apenas 3 rotas foram mapeadas
    
    #6

---
+++ swagger.ts
@@ -0,0 +1,20 @@
+import { Response, Request } from "express";
+
+const express = require('express');
+const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
