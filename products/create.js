6148837869bb5b24d13ed6889fbb2e89872a4709
gvarela <guilhermevareladossantos@gmail.com>
2023-11-30 23:08:34 -0300

    Atualiza documentação
    melhorando os parâmetros da rota e atualizando o URL base da API
    
    #4

--- products/create.js
+++ products/create.js
@@ -7,7 +7,7 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 module.exports.create = (event, context, callback) => {
     const timestamp = new Date().getTime();
     const data = JSON.parse(event.body);
-    if (typeof data.text !== 'string') {
+    if (typeof data.name !== 'string') {
         console.error('Validation Failed');
         callback(null, {
             statusCode: 400,
@@ -21,7 +21,7 @@ module.exports.create = (event, context, callback) => {
         TableName: process.env.DYNAMODB_TABLE,
         Item: {
             id: data.id,
-            text: data.text,
+            name: data.name,
             checked: false,
             createdAt: timestamp,
             updatedAt: timestamp,
